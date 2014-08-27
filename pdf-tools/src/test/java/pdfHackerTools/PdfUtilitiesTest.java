42cf57d058f4790e36789bdd86fa6a79a50447da
Carl Wilson <carl@openplanetsfoundation.org>
2014-08-27 12:32:36 +0100

    Added mechanism to get test file to unit tests.

--- pdf-tools/src/test/java/pdfHackerTools/PdfUtilitiesTest.java
+++ pdf-tools/src/test/java/pdfHackerTools/PdfUtilitiesTest.java
@@ -1,10 +1,21 @@
 package pdfHackerTools;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.net.URISyntaxException;
 
 import org.junit.Test;
 
 public class PdfUtilitiesTest {
+	public static final File ISATOR_MANUAL;
+	static {
+		try {
+			ISATOR_MANUAL = PdfUtilitiesTest.getResourceAsFile("pdfHackerTools/Isartor_test_suite_manual.pdf");
+		} catch (URISyntaxException excep) {
+			throw new AssertionError("Couldn't load test data.");
+		}
+	}
 
 	@Test
 	public final void testGetPaths() {
@@ -19,6 +30,11 @@ public class PdfUtilitiesTest {
 	@Test
 	public final void testCheckPdfSizeString() {
 		fail("Not yet implemented"); // TODO
+	}
+
+	public final static File getResourceAsFile(String resName)
+			throws URISyntaxException {
+		return new File(ClassLoader.getSystemResource(resName).toURI());
 	}
 
 }
