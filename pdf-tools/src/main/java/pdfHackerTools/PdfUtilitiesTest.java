d986337e973bd8230f04fb762f07679f5d98df49
Carl Wilson <carl@openplanetsfoundation.org>
2014-08-27 11:48:47 +0100

    Added empty Junit tests for PDFUtilities.

---
+++ pdf-tools/src/main/java/pdfHackerTools/PdfUtilitiesTest.java
@@ -0,0 +1,24 @@
+package pdfHackerTools;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PdfUtilitiesTest {
+
+	@Test
+	public final void testGetPaths() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCheckPdfSizeFile() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCheckPdfSizeString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
