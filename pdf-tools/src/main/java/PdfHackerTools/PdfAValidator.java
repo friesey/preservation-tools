5b1bef53f1188d8c3e97a98dc0249be3b66d9ca0
friesey <y.friese@zbw.eu>
2014-08-27 10:24:03 +0200

    cleaned up the Code in "PdfAValidator"

--- pdf-tools/src/main/java/PdfHackerTools/PdfAValidator.java
+++ pdf-tools/src/main/java/PdfHackerTools/PdfAValidator.java
@@ -26,10 +26,12 @@ public class PdfAValidator {
 		try {
 
 			t = PdfUtilities.chooseFolder();
-			
+
+			// Generating two Outputfiles in the folder that is examined
+
 			outputfile = new PrintWriter(new FileWriter(t + "//"
 					+ "PdfAValidation.txt"));
-			
+
 			ShortSummary = new PrintWriter(new FileWriter(t + "//"
 					+ "PdfAValidationShortSummary.txt"));
 
@@ -40,105 +42,104 @@ public class PdfAValidator {
 
 				for (int i = 0; i < files.size(); i++) {
 					if (!files.get(i).isDirectory() && files.get(i) != null) {
+
 						System.out.println(i + 1);
 						outputfile.println(i + 1);
 						ShortSummary.println(i + 1);
 
 						try {
-
 							System.out.println(files.get(i).getCanonicalPath());
 							outputfile.println(files.get(i).getCanonicalPath());
-							ShortSummary.println(files.get(i).getCanonicalPath());	
+							ShortSummary.println(files.get(i)
+									.getCanonicalPath());
 
-										if (PdfUtilities.testPdfOk(files.get(i))) {
+							if (PdfUtilities.testPdfOk(files.get(i)))
+							/*
+							 * Test if the Pdf File is ok to be examined.
+							 * Otherwise gives error in Console
+							 */
+							{
+								String PdfType = PdfUtilities.checkIfPdfA(files
+										.get(i));
+								if (PdfType.contains("PDF/A")) {
+									/*
+									 * the actual PdfAValidation starts here
+									 */
+									ValidationResult result = null;
+									FileDataSource fd = new FileDataSource(
+											files.get(i).toString());
+									PreflightParser parser = new PreflightParser(
+											fd);
+									try {
+										parser.parse();
+										PreflightDocument document = parser
+												.getPreflightDocument();
+										try {
+											document.validate();
 
-											String PdfType = PdfUtilities
-													.checkIfPdfA(files.get(i));
-											if (PdfType.contains("PDF/A")) {
-												// the actual PdfAValidation
-												// starts here
-												ValidationResult result = null;
-												FileDataSource fd = new FileDataSource(
-														files.get(i).toString());
-												PreflightParser parser = new PreflightParser(
-														fd);
-												try {
-													parser.parse();
+											result = document.getResult();
+											document.close();
 
-													PreflightDocument document = parser
-															.getPreflightDocument();
-													try {
-														document.validate();
+										} catch (NullPointerException e) {
+											/*
+											 * TODO: Why can this generate a
+											 * NullPointerException ?
+											 */
+											outputfile.println(e);
+											ShortSummary.println(e);
+										}
+									} catch (SyntaxValidationException e) {
+										/*
+										 * the parse method throws a
+										 * SyntaxValidationException if the PDF
+										 * file can't be parsed.
+										 */
+										result = e.getResult();
+									}
+									if (result != null) {
+										if (result.isValid()) {
+											outputfile
+													.println("The file "
+															+ files.get(i)
+															+ " is a valid PDF/A-1b file");
 
-														result = document
-																.getResult();
-														document.close();
+											ShortSummary
+													.println("The file "
+															+ files.get(i)
+															+ " is a valid PDF/A-1b file");
+										} else {
+											outputfile
+													.println("The file "
+															+ files.get(i)
+															+ " is not valid, error(s) :");
 
-													} catch (NullPointerException e) {
-														outputfile.println(e);
-														ShortSummary.println(e);
-													}
-												} catch (SyntaxValidationException e) {
-													/*
-													 * the parse method throws a
-													 * SyntaxValidationException
-													 * if the PDF file can't be
-													 * parsed.
-													 */
-													result = e.getResult();
-												}
-												if (result != null) {
-													if (result.isValid()) {
-														outputfile
-																.println("The file "
-																		+ files.get(i)
-																		+ " is a valid PDF/A-1b file");
-														
-														ShortSummary
-														.println("The file "
-																+ files.get(i)
-																+ " is a valid PDF/A-1b file");
-													} else {
-														outputfile
-																.println("The file "
-																		+ files.get(i)
-																		+ " is not valid, error(s) :");
-														
-														ShortSummary.println("The file "
-																+ files.get(i)
-																+ " is not valid, error(s) :");														
-														for (ValidationError error : result
-																.getErrorsList()) {
-															outputfile
-																	.println(error
-																			.getErrorCode()
-																			+ " : "
-																			+ error.getDetails());
-														}
-													}
-												}
+											ShortSummary
+													.println("The file "
+															+ files.get(i)
+															+ " is not valid, error(s) :");
+											for (ValidationError error : result
+													.getErrorsList()) {
+												outputfile.println(error
+														.getErrorCode()
+														+ " : "
+														+ error.getDetails());
 											}
-
-											else {
-												// outputfile.println(PdfType);
-												ShortSummary.println("No PDF/A file");
-												outputfile.println("No PDF/A file");
-											}
-										}								
+										}
+									}
+								} else {
+									ShortSummary.println("No PDF/A file");
+									outputfile.println("No PDF/A file");
 								}
-							
-					catch (IOException e) {
+							}
+						} catch (IOException e) {
 							outputfile.print(e);
 						}
 					}
 				}
 			}
-			
 			ShortSummary.close();
 			outputfile.close();
-		} 
-		
-		catch (FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 		}
 	}
 }
