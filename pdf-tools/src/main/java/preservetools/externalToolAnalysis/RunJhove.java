cc99447b757d8ea0226f698bc44cb896b5e1c459
friesey <y.friese@zbw.eu>
2014-09-22 15:14:10 +0200

    implemented a class which can run JHOVE (starting small with just opening JHOVE)

--- pdf-tools/src/main/java/preservetools/externalToolAnalysis/RunJhove.java
+++ pdf-tools/src/main/java/preservetools/externalToolAnalysis/RunJhove.java
@@ -1,7 +1,28 @@
 package preservetools.externalToolAnalysis;
 
+import java.io.IOException;
+
 public class RunJhove {
-	
-	//path: C:\Users\Friese Yvonne\jhove-1_11_Schmitt\jhove\bin//JhoveView
+
+	static String JhovePath;
+
+	public static void main(String args[]) throws IOException {
+
+		JhovePath = "C://Users//Friese Yvonne//jhove-1_11//jhove//bin//JhoveView.jar";
+
+		try {
+
+			ProcessBuilder startJhove = new ProcessBuilder("java", "-Xmx1024m", "-Xms1024m",
+				    "-DTOOLS_DIR=/home/IM/work/dist", "-Daoi=whole", 
+				    "-jar", JhovePath);
+			
+			startJhove.start();
+		}
+
+		catch (IOException e) {
+			System.out.println(e);
+		}
+
+	}
 
 }
