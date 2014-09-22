6ea80b037d2cf0e67feaff930b2bf26c9424a795
friesey <y.friese@zbw.eu>
2014-09-22 14:27:02 +0200

    began to edit a tiff file analysis and bundle generic file tests in "GenericFileAnalysis"-class

--- pdf-tools/src/main/java/preservetools/files/GenericFileAnalysis.java
+++ pdf-tools/src/main/java/preservetools/files/GenericFileAnalysis.java
@@ -1,6 +1,89 @@
 package preservetools.files;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class GenericFileAnalysis {
 
-	//TODO: If I do not know yet which file it is, just a generic analyse
+	// TODO: If I do not know yet which file it is, just a generic analyse
+
+	static BufferedReader fileReader;
+	static String magicNumberPdf = "%PDF";
+	static String magicNumberTiffIntel = "II";
+	static String magicNumberTiffMotorola = "MM";
+
+	/**
+	 * Tests if the first line of the file contains the proper PDF-Header "%PDF"
+	 * For Strings
+	 *
+	 * @param Creates
+	 *            a PdfHeaderTest-Pdf-Reader and reads the first line of the
+	 *            PDF-file like an editor would do. Overloaded with the data
+	 *            type "String"
+	 * @return: boolean false = no PDF-Header; true = first line contains
+	 *          PDF-Header
+	 */
+	public static boolean testFileHeaderPdf(String file) throws IOException {
+		fileReader = new BufferedReader(new FileReader(file));
+		String FileHeader = fileReader.readLine();
+		// System.out.println(FileHeader);
+		if (FileHeader != null) {
+			if (FileHeader.contains("magicNumberPdf")) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+	
+	
+	public static boolean testFileHeaderPdf(File file) throws IOException {
+		fileReader = new BufferedReader(new FileReader(file));
+		String FileHeader = fileReader.readLine();
+		// System.out.println(FileHeader);
+		if (FileHeader != null) {
+			if (FileHeader.contains("magicNumberPdf")) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean testFileHeaderTiff(String file) throws IOException {
+		fileReader = new BufferedReader(new FileReader(file));
+		String FileHeader = fileReader.readLine();
+		// System.out.println(FileHeader);
+		if (FileHeader != null) {
+			if ((FileHeader.contains(magicNumberTiffIntel)) || (FileHeader.contains(magicNumberTiffMotorola))) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+	
+	public static boolean testFileHeaderTiff(File file) throws IOException {
+		fileReader = new BufferedReader(new FileReader(file));
+		String FileHeader = fileReader.readLine();
+		// System.out.println(FileHeader);
+		if (FileHeader != null) {
+			if ((FileHeader.contains(magicNumberTiffIntel)) || (FileHeader.contains(magicNumberTiffMotorola))) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
 }
--- pdf-tools/src/main/java/preservetools/files/tiff/TiffTagAnalysis.java
+++ pdf-tools/src/main/java/preservetools/files/tiff/TiffTagAnalysis.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
+import org.apache.commons.io.FilenameUtils;
+
 public class TiffTagAnalysis {
 
 	static String examinedFolder;
@@ -16,23 +18,39 @@ public class TiffTagAnalysis {
 		try {
 
 			examinedFolder = preservetools.utilities.FolderBrowserDialog.chooseFolder();
-			
-			 if (examinedFolder != null) {
-				 
-				 ArrayList<File> files = preservetools.utilities.ListsFiles.getPaths(new File(examinedFolder),
-						 new ArrayList<File>());
-				 
-				 for (int i = 0; i < files.size(); i++) {
-					 System.out.println(files.get(i).getCanonicalPath());					 
-					 
-				 }				 
-				 
-			 }
-			
 
+			if (examinedFolder != null) {
+
+				ArrayList<File> files = preservetools.utilities.ListsFiles.getPaths(new File(examinedFolder),
+						new ArrayList<File>());
+
+				for (int i = 0; i < files.size(); i++) {
+					System.out.println(files.get(i).getCanonicalPath());
+
+					String tiffExtension = "TIF";
+
+					String extension = FilenameUtils.getExtension(files.get(i).getCanonicalPath());
+					if (extension.equals(tiffExtension)) {
+						
+						if (preservetools.files.GenericFileAnalysis.testFileHeaderTiff(files.get(i))) {
+							
+							
+							System.out.println ( files.get(i) + "is a tiff-file");
+							
+							
+						}
+						else {
+							System.out.println ("This file purports to be a tiff-file. It has a .TIF-extension, but no lacks the magic number.");
+							
+							
+						}
+					}
+
+				}
+
+			}
 		} catch (FileNotFoundException e) {
 		}
 
 	}
-
 }
