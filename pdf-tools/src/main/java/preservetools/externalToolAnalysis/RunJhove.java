8513b1f26a13e31e397a1fc3b1f14c8a39784890
friesey <y.friese@zbw.eu>
2014-09-22 15:38:55 +0200

    edited RunJhove, input and output still to be build

--- pdf-tools/src/main/java/preservetools/externalToolAnalysis/RunJhove.java
+++ pdf-tools/src/main/java/preservetools/externalToolAnalysis/RunJhove.java
@@ -1,28 +1,44 @@
 package preservetools.externalToolAnalysis;
 
+import java.io.File;
 import java.io.IOException;
+import java.lang.ProcessBuilder.Redirect;
 
 public class RunJhove {
 
 	static String JhovePath;
 
-	public static void main(String args[]) throws IOException {
+	public static void main(String args[]) throws IOException, InterruptedException {
 
 		JhovePath = "C://Users//Friese Yvonne//jhove-1_11//jhove//bin//JhoveView.jar";
 
 		try {
 
-			ProcessBuilder startJhove = new ProcessBuilder("java", "-Xmx1024m", "-Xms1024m",
-				    "-DTOOLS_DIR=/home/IM/work/dist", "-Daoi=whole", 
-				    "-jar", JhovePath);
+			ProcessBuilder runsJhove = new ProcessBuilder("java", "-Xmx1024m", "-Xms1024m",
+					"-DTOOLS_DIR=/home/IM/work/dist", "-Daoi=whole", "-jar", JhovePath);
+
+			Process p = runsJhove.start();
 			
-			startJhove.start();
+			File testPdf = new File("C://PresToolsTest//Input.txt");
+			
+			p.getInputStream();
+			
+			assert runsJhove.redirectInput() == Redirect.from(testPdf);
+
+			File log = new File("C://PresToolsTest//log.txt");
+			
+			// TODO: Find out how to put files/folder in runsJHOVE and how to get the output
+
+			runsJhove.redirectErrorStream(true);
+			runsJhove.redirectOutput(Redirect.appendTo(log));
+
+			
+			p.waitFor();			
+			p.destroy();
 		}
 
 		catch (IOException e) {
 			System.out.println(e);
 		}
-
 	}
-
 }
