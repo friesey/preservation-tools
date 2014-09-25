21af1360b8d058eeb5b5f4d37f4d058e826d7426
friesey <y.friese@zbw.eu>
2014-09-25 15:41:01 +0200

    edit CdRomIsoImageChecker class: choose CdRom. List Files.

--- pdf-tools/src/main/java/preservetools/files/executables/CdRom_IsoImageChecker.java
+++ pdf-tools/src/main/java/preservetools/files/executables/CdRom_IsoImageChecker.java
@@ -1,13 +1,26 @@
 package preservetools.files.executables;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class CdRom_IsoImageChecker {
 
-	public static void main(String args[]) throws FileNotFoundException {
-		
-		
-		// Test files C:\FileSample\CDROMs
+	static String examinedCdRom;
 
+	public static void main(String args[]) throws FileNotFoundException {
+
+		examinedCdRom = preservetools.utilities.FolderBrowserDialog
+				.chooseFolder();
+
+		if (examinedCdRom != null) {
+
+			ArrayList<File> files = preservetools.utilities.ListsFiles
+					.getPaths(new File(examinedCdRom), new ArrayList<File>());
+
+			for (int i = 0; i < files.size(); i++) {
+
+			}
+		}
 	}
 }
