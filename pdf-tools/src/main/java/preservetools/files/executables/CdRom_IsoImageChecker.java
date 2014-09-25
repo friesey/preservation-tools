ee2d0583d519e41151daeb233012060ee527441d
friesey <y.friese@zbw.eu>
2014-09-25 15:29:03 +0200

    edited a tool that checks if a CD ROM contains executable Files and therefor an ISO-Image should be generated to archive the content of the CD ROM

---
+++ pdf-tools/src/main/java/preservetools/files/executables/CdRom_IsoImageChecker.java
@@ -0,0 +1,13 @@
+package preservetools.files.executables;
+
+import java.io.FileNotFoundException;
+
+public class CdRom_IsoImageChecker {
+
+	public static void main(String args[]) throws FileNotFoundException {
+		
+		
+		// Test files C:\FileSample\CDROMs
+
+	}
+}
