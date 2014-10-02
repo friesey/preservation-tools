974366f00bb62407d3603f8e062e37617d47621d
friesey <y.friese@zbw.eu>
2014-10-02 16:13:26 +0200

    changed function ListsFiles because it should be used generically

--- pdf-tools/src/main/java/preservetools/utilities/ListsFiles.java
+++ pdf-tools/src/main/java/preservetools/utilities/ListsFiles.java
@@ -26,16 +26,17 @@ public class ListsFiles {
 		for (File f : fileArr) {
 			// TODO If a folder is chosen that cannot be searched/read, e. g.
 			// C:/, the tool runs into issues
-			if (f.isDirectory()) {
-		
-				if (!f.getName().toLowerCase().contains ("adobereader")) {
-				getPaths(f, list);
-				}
-				
-				else {
-					System.out.println ("Adobe Reader Software Folder:" + f.toString().toLowerCase());
-				}
-			}
+
+			// TODO: This cannot be done in this generic method. It's only for the Cdom_IsoImageChecker
+			/*
+			 * if (f.isDirectory()) { if (!f.getName().toLowerCase().contains
+			 * ("adobereader")) { getPaths(f, list); }
+			 * 
+			 * else { System.out.println ("Adobe Reader Software Folder:" +
+			 * f.toString().toLowerCase()); }
+			 * 
+			 * }
+			 */
 			if (!f.isDirectory()) { // adds only non-directories (=files) to the
 									// ArrayList of Files
 
@@ -45,13 +46,13 @@ public class ListsFiles {
 				if (extension != null) {
 
 					// TODO: What about .tar-files etc?
-					if (extension.equals("zip")) {				
+					if (extension.equals("zip")) {
 						try {
 							ZipFile zf = new ZipFile(f.toString());
 							Enumeration<? extends ZipEntry> e = zf.entries();
 							ZipEntry ze;
 							while (e.hasMoreElements()) {
-								ze = e.nextElement();						
+								ze = e.nextElement();
 
 								if (!ze.isDirectory()) {
 									File CompFile = new File(ze.toString());
@@ -70,7 +71,7 @@ public class ListsFiles {
 						}
 					}
 
-					else {		
+					else {
 
 						list.add(f);
 					}
@@ -82,18 +83,21 @@ public class ListsFiles {
 									+ f);
 					list.add(f);
 				}
-				
-				//remove all Adobe Reader setup files from list
-				
+
+				// remove all Adobe Reader setup files from list
+
 				String acrobatreadertest = f.toString().toLowerCase();
-				
-				//TODO: ingore complete folder named "AdobeReader"
-				
-				if ((acrobatreadertest.contains("adberdr")) || (acrobatreadertest.contains("acroread")) || (acrobatreadertest.contains("adobe_acrobat_reader"))) {
-					System.out.println ("Adobe Reader Software:" + f.toString().toLowerCase());
-					 list.remove(f);
+
+				// TODO: ingore complete folder named "AdobeReader"
+
+				if ((acrobatreadertest.contains("adberdr"))
+						|| (acrobatreadertest.contains("acroread"))
+						|| (acrobatreadertest.contains("adobe_acrobat_reader"))) {
+					System.out.println("Adobe Reader Software:"
+							+ f.toString().toLowerCase());
+					list.remove(f);
 				}
-				
+
 			}
 		}
 
