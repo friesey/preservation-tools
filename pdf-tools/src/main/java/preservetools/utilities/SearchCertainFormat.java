8b4f70106a4780746faefb03948a52977aa9ce83
friesey <y.friese@zbw.eu>
2014-10-17 10:56:55 +0200

    edited new handy Class to search for certain file formats or mime types

---
+++ pdf-tools/src/main/java/preservetools/utilities/SearchCertainFormat.java
@@ -0,0 +1,52 @@
+package preservetools.utilities;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+import org.apache.commons.io.FilenameUtils;
+
+public class SearchCertainFormat {
+
+	static String examinedFolder;
+
+	public static void main(String args[]) throws IOException {
+
+		try {
+			JOptionPane.showMessageDialog(null, "CD ROM Dialog",
+					"Please choose Folder",
+					JOptionPane.QUESTION_MESSAGE);
+			examinedFolder = preservetools.utilities.FolderBrowserDialog
+					.chooseFolder();
+
+			if (examinedFolder != null) {
+
+				ArrayList<File> files = preservetools.utilities.ListsFiles
+						.getPaths(new File(examinedFolder),
+								new ArrayList<File>());
+
+				for (int i = 0; i < files.size(); i++) {
+					
+					String extension = FilenameUtils.getExtension(
+							files.get(i).toString()).toLowerCase();
+					
+					if (extension != null) {
+						if (extension.equals("wav")) {
+							System.out.println(files.get(i));
+						}
+					}
+					
+					else {
+					//	System.out.println ("another extension");
+					}
+
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
