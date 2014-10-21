ed93128ec1b360fed970364dda3883ee9356b08e
friesey <y.friese@zbw.eu>
2014-10-21 14:39:39 +0200

    editing Class SearchForStringinPDFFiles

--- pdf-tools/src/main/java/preservetools/files/pdf/SearchforStringinPDFFiles.java
+++ pdf-tools/src/main/java/preservetools/files/pdf/SearchforStringinPDFFiles.java
@@ -1,7 +1,10 @@
 package preservetools.files.pdf;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class SearchforStringinPDFFiles {
 	
@@ -10,8 +13,27 @@ public class SearchforStringinPDFFiles {
 	static PrintWriter outputfile;
 
 	public static void main(String args[]) throws IOException {
+		try {
 		
+		folder = preservetools.utilities.FolderBrowserDialog.chooseFolder();
 		
-	}
+		if (folder != null) {
+			
+	
+				ArrayList<File> files = preservetools.utilities.ListsFiles
+						.getPaths(new File(folder),
+								new ArrayList<File>());
+				if (files == null) {
+					
+				}
+					
+					
+		}
+		}
 
+			catch (FileNotFoundException e) {
+				System.out.println(e);
+
+			}
+}
 }
