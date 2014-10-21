8f883a69e959b0f818a8b33f4f8c89487bdbfc84
friesey <y.friese@zbw.eu>
2014-10-21 14:37:03 +0200

    created SearchForStringinPDFFile

---
+++ pdf-tools/src/main/java/preservetools/files/pdf/SearchforStringinPDFFiles.java
@@ -0,0 +1,17 @@
+package preservetools.files.pdf;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SearchforStringinPDFFiles {
+	
+	static String folder;
+
+	static PrintWriter outputfile;
+
+	public static void main(String args[]) throws IOException {
+		
+		
+	}
+
+}
