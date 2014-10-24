2892f838de44e136707fc5132bca93f709242305
friesey <y.friese@zbw.eu>
2014-10-24 12:58:35 +0200

    implemented ability to search wordfiles

--- pdf-tools/src/main/java/filetools/pdf/SearchforStringinPdfFiles.java
+++ pdf-tools/src/main/java/filetools/pdf/SearchforStringinPdfFiles.java
@@ -1,15 +1,22 @@
 package filetools.pdf;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JOptionPane;
 
 import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 
 public class SearchforStringinPdfFiles {
 
@@ -20,18 +27,18 @@ public class SearchforStringinPdfFiles {
 	static PrintWriter outputfile;
 
 	public static void main(String args[]) throws IOException {
-		
+
 		String extension;
-	
+
 		System.out.println("Test");
 
-		try {			
-			
+		int stringfound = 0;
+
+		try {
+
 			folder = utilities.FolderBrowserDialog.chooseFolder();
-			
-			PrintWriter outputfile = new PrintWriter(new FileWriter(folder + "//"
-					+ "SearchForString" + ".txt"));
-			
+
+			PrintWriter outputfile = new PrintWriter(new FileWriter(folder + "//" + "SearchForString" + ".txt"));
 
 			searchedString = JOptionPane.showInputDialog(null, "Please enter String that should be searched in the PDF Files", "Enter String Mask", JOptionPane.PLAIN_MESSAGE);
 
@@ -46,11 +53,10 @@ public class SearchforStringinPdfFiles {
 						// for lazy & hurry reasons
 
 						for (int i = 0; i < files.size(); i++) {
-							
-							extension = FilenameUtils.getExtension(files.get(i).toString())
-									.toLowerCase();
 
-							if (extension.equals ("pdf")) {
+							extension = FilenameUtils.getExtension(files.get(i).toString()).toLowerCase();
+
+							if (extension.equals("pdf")) {
 
 								String[] linesPdf = PdfAnalysis.extractsPdfLines(files.get(i).toString());
 
@@ -58,27 +64,53 @@ public class SearchforStringinPdfFiles {
 
 								for (int j = 0; j < lenlines; j++) {
 									if ((linesPdf[j]).contains(searchedString)) {
+										stringfound++;
 										outputfile.println();
 										outputfile.println(files.get(i));
 										outputfile.println(linesPdf[j]);
-										//TODO: I wonder why the lines are so high. Is this calculating cumulative?
-										outputfile.println("Line: " + j);
 										outputfile.println();
 									}
 								}
 							}
-							
-							//TODO: Add other file formats, e. g. MS Word to search for string there, too
+
+							if ((extension.equals("doc")) || (extension.equals("docx"))) {
+								InputStream wordstream = new FileInputStream(files.get(i));
+								XWPFDocument wordfile = new XWPFDocument(wordstream);
+
+								List<XWPFParagraph> allparagraphs = wordfile.getParagraphs();
+								int parCount = allparagraphs.size();
+								System.out.println(parCount);
+
+								for (int n = 0; n < parCount; n++) {
+									String line = allparagraphs.get(n).getParagraphText();
+
+									if (line.contains(searchedString)) {
+										stringfound++;
+										outputfile.println();
+										outputfile.println(files.get(i));
+										outputfile.println("Searched String found in line/paragraph :" + n);
+										outputfile.println(allparagraphs.get(n));
+										outputfile.println();
+									}
+								}
+							}
+
+							// TODO: Add other file formats, e. g. MS Word to
+							// search for string there, too
 
 							else {
-							//	System.out.println(files.get(i) + " is not a PDF file");
+								// System.out.println(files.get(i) +
+								// " is not a PDF file");
 							}
 						}
 					}
 
 				}
 			}
-			
+			if (stringfound == 0) {
+				outputfile.println("The searched String was not found");
+			}
+			outputfile.println("The searched String was found " + stringfound + " times");
 			outputfile.close();
 			System.out.println("File was closed");
 		}
