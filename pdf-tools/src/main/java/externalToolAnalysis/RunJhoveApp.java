f9bd09f831adc2235680ce7ffb77df2f6482a4d1
friesey <y.friese@zbw.eu>
2014-11-17 15:54:01 +0100

    added one class. But Library is not part of maven, this might cause problems.

---
+++ pdf-tools/src/main/java/externalToolAnalysis/RunJhoveApp.java
@@ -0,0 +1,70 @@
+package externalToolAnalysis;
+
+import java.io.FileNotFoundException;
+
+import edu.harvard.hul.ois.jhove.App;
+import edu.harvard.hul.ois.jhove.JhoveBase;
+import edu.harvard.hul.ois.jhove.JhoveException;
+import edu.harvard.hul.ois.jhove.Module;
+import edu.harvard.hul.ois.jhove.OutputHandler;
+import edu.harvard.hul.ois.jhove.handler.XmlHandler;
+import edu.harvard.hul.ois.jhove.module.PdfModule;
+
+/* the libray JhoveApp.jar is not in the maven library*/
+
+public class RunJhoveApp {
+
+	public static void main(String args[]) throws JhoveException, FileNotFoundException {
+
+		try {
+
+			String outputFile = utilities.FileBrowserDialog.chooseFile();
+			/*
+			 * Findings will be written here
+			 */
+
+			String folder = utilities.FolderBrowserDialog.chooseFolder();
+			/*
+			 * Choosing Folder which is to be examined
+			 */
+
+			JhoveBase jb = new JhoveBase();
+
+			//der Path stimmt so noch nicht
+			String configFilePath = "C:\\Users\\Friese Yvonne\\jhove-1_11\\jhove\\conf\\jhove.conf";
+
+			jb.init(configFilePath.toString(), null);
+
+			jb.setEncoding("UTF-8"); /*
+									 * UTF-8 does not calculate checksums, which
+									 * saves time
+									 */
+			jb.setTempDirectory("/user/me/temp1");
+			jb.setBufferSize(131072);
+			jb.setChecksumFlag(false);
+			jb.setShowRawFlag(false);
+			jb.setSignatureFlag(false);
+
+			String appName = "Yvonne JHOVE wrapper";
+			String version = "1.0";
+			int[] date = { 2014, 11, 17 };
+			// year, month, day
+			String usage = "Call JHOVE via own Java";
+			String rights = "Copyright nestor Format Working Group";
+			App app = new App(appName, version, date, usage, rights);
+
+			Module module = new PdfModule(); /* to try this with PdfModule only */
+
+			OutputHandler handler = new XmlHandler();
+
+			jb.process(app, module, handler, folder);
+
+		}
+
+		catch (Exception e) {
+			System.out.println("Exception: " + e);
+		}
+
+	}
+
+}
---
+++ pdf-tools/src/main/java/utilities/SearchforStringinjavaFiles.java
@@ -0,0 +1,116 @@
+package utilities;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+
+import filetools.pdf.PdfAnalysis;
+
+public class SearchforStringinjavaFiles {
+	
+	static String folder;
+	static String searchedString;
+	static PrintWriter outputfile;
+
+	public static void main(String args[]) throws IOException {
+
+		String extension;
+		int stringfound = 0;
+
+		try {
+
+			folder = utilities.FolderBrowserDialog.chooseFolder();
+
+			PrintWriter outputfile = new PrintWriter(new FileWriter(folder + "//" + "SearchForString" + ".txt"));
+
+			searchedString = JOptionPane.showInputDialog(null, "Please enter String that should be searched in the PDF Files", "Enter String Mask", JOptionPane.PLAIN_MESSAGE);
+
+			if (searchedString != null) {
+
+				if (folder != null) {
+
+					ArrayList<File> files = utilities.ListsFiles.getPaths(new File(folder), new ArrayList<File>());
+					if (files != null) {
+
+						for (int i = 0; i < files.size(); i++) {
+
+							String filename = FilenameUtils.getBaseName(files.get(i).toString());
+
+							if (!filename.startsWith("~")) {
+								extension = FilenameUtils.getExtension(files.get(i).toString()).toLowerCase();
+											
+														 if ((extension.equals("txt")) || (extension.equals("java"))) {
+									if (files.get(i).length() != 0)/*
+																	 * important
+																	 * because
+																	 * otherwise
+																	 * not yet
+																	 * closed
+																	 * outpufile
+																	 * causes
+																	 * neverending
+																	 * story
+																	 */{
+										// TODO: There is a big performance
+										// problem with too large Txt-Files, e.
+										// g. more than 500 KB or a certain no.
+										// of lines.
+										BufferedReader txtreader = new BufferedReader(new FileReader(files.get(i)));
+										String line;
+										while (null != (line = txtreader.readLine())) {
+											if (line.contains(searchedString)) {
+												stringfound++;
+												outputfile.println();
+												outputfile.println(files.get(i));
+												outputfile.println(line);
+												outputfile.println();
+											}
+										}
+										txtreader.close();
+									}
+								}
+
+								else {
+									System.out.println(files.get(i).toString());
+								}
+							}
+						}
+						}
+					}
+				}
+
+				// TODO: Add other file formats, e. g. MS Word to
+				// search for string there, too
+
+				else {
+					// System.out.println(files.get(i) +
+					// " is not a PDF file");
+				}		
+
+			if (stringfound == 0) {
+				outputfile.println("The searched String was not found");
+			}
+			outputfile.println("The searched String was found in " + stringfound + " paragraphs");
+			outputfile.close();
+			}
+		 catch (FileNotFoundException e) {
+			System.out.println(e);
+		}
+	}
+}
+
+
