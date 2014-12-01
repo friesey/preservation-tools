9fd97de334a93307059c4ea8d564897a7c246267
friesey <y.friese@zbw.eu>
2014-12-01 12:00:32 +0100

    test if Travis is ok if RunJhoveApp is commented

--- pdf-tools/src/main/java/externalToolAnalysis/RunJhoveApp.java
+++ pdf-tools/src/main/java/externalToolAnalysis/RunJhoveApp.java
@@ -1,4 +1,4 @@
-package externalToolAnalysis;
+/*package externalToolAnalysis;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -38,9 +38,9 @@ public class RunJhoveApp {
 				jb.init(configFilePath, null);
 
 				jb.setEncoding("UTF-8");
-				/*
+				
 				 * * UTF-8 does not calculate checksums, which saves time
-				 */
+				 
 				jb.setTempDirectory("C://temp1");
 				jb.setBufferSize(131072);
 				jb.setChecksumFlag(false);
@@ -56,9 +56,9 @@ public class RunJhoveApp {
 				App app = new App(appName, version, date, usage, rights);
 
 				Module module = new PdfModule();
-				/*
+				
 				 * * to try this with PdfModule only
-				 */
+				 
 				OutputHandler handler = new XmlHandler();
 
 				ArrayList<File> files = utilities.ListsFiles.getPaths(new File(folder), new ArrayList<File>());
@@ -89,7 +89,7 @@ public class RunJhoveApp {
 
 						// TODO: This does not work yet. Would be a great idea,
 						// though.
-						/*
+						
 						 * URL url = files.get(i).toURI().toURL(); RepInfo
 						 * infoobject = new RepInfo(url.toExternalForm());
 						 * List<Message> messages = infoobject.getMessage();
@@ -97,7 +97,7 @@ public class RunJhoveApp {
 						 * while (iterator.hasNext()) {
 						 * System.out.println(iterator.next()); } String format
 						 * = infoobject.getFormat();
-						 */
+						 
 						writer.println("</item>");
 					}
 				}
@@ -126,4 +126,4 @@ public class RunJhoveApp {
 
 	}
 
-}
+}*/
