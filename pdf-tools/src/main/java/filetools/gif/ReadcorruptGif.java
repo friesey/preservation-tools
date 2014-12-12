ddbe2da2ed90cbb3fa471225fc18a8c96bf28c4d
friesey <y.friese@zbw.eu>
2014-12-12 13:14:28 +0100

    this code works for corrupted images but the new image is as damaged as the old one

--- pdf-tools/src/main/java/filetools/gif/ReadcorruptGif.java
+++ pdf-tools/src/main/java/filetools/gif/ReadcorruptGif.java
@@ -1,10 +1,6 @@
 package filetools.gif;
 
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
-import java.awt.image.ImageObserver;
 import java.io.File;
 
 import javax.imageio.ImageIO;
@@ -12,9 +8,10 @@ import javax.imageio.stream.ImageInputStream;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 
-import org.apache.sanselan.Sanselan;
 import org.apache.sanselan.formats.gif.GifImageParser;
 
 public class ReadcorruptGif {
@@ -30,18 +27,23 @@ public class ReadcorruptGif {
 		System.out.println(xmp);*/
 
 		// try {
-
-		File sourceimage = new File(giffile);
-
+	
 		InputStream is = new BufferedInputStream(new FileInputStream(giffile));
-		ImageInputStream iis = ImageIO.createImageInputStream(is);
+	//	ImageInputStream iis = ImageIO.createImageInputStream(is);
 
-		Image toolkitImage = Toolkit.getDefaultToolkit().getImage(giffile); // this works	
+	//	Image toolkitImage = Toolkit.getDefaultToolkit().getImage(giffile); // this works			
+		
+		GifImageParser parser = new GifImageParser();
+		
+		File outputImg = new File(gif.getParent().toString() + "//modifiedGif_" + gif.getName().toString() + ".gif");
+		OutputStream stream = new FileOutputStream(outputImg);		
+		parser.copyStreamToStream(is, stream);
+		
+		ImageInputStream iis = ImageIO.createImageInputStream(stream);
 
 		BufferedImage bufImage = ImageIO.read(iis); // this only works for
 													// intact images
-
-		File outputImg = new File(gif.getParent().toString() + "//modifiedGif_" + gif.getName().toString() + ".gif");
+		
 		ImageIO.write(bufImage, "gif", outputImg);
 
 		/*
