40cece5ce5cd57e55715ac3c559ce8c2cfc5f8ba
friesey <y.friese@zbw.eu>
2014-12-12 12:04:39 +0100

    code works for intact gif files but not for corrupted

--- pdf-tools/src/main/java/filetools/gif/ReadcorruptGif.java
+++ pdf-tools/src/main/java/filetools/gif/ReadcorruptGif.java
@@ -1,19 +1,15 @@
 package filetools.gif;
 
-import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
 import java.io.File;
-import java.net.URL;
-import java.util.Iterator;
 
-import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
-import javax.imageio.ImageWriteParam;
-import javax.imageio.ImageWriter;
-import javax.imageio.stream.FileImageOutputStream;
+import javax.imageio.stream.ImageInputStream;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 public class ReadcorruptGif {
 
@@ -23,47 +19,25 @@ public class ReadcorruptGif {
 
 		File gif = new File(giffile);
 
-		try {
-			/*
-			 * URL url = new URL("file://" + gif.getPath());
-			 * System.out.println(url); BufferedImage img = ImageIO.read(url);
-			 */
+	//	try {
 
-			BufferedImage gifbufimg = getBufferedImage(gif);
+			File sourceimage = new File(giffile);
 
-			Iterator<ImageWriter> iter = ImageIO.getImageWritersByFormatName("gif");
-			ImageWriter writer = (ImageWriter) iter.next();
-			ImageWriteParam iwp = writer.getDefaultWriteParam();
+			InputStream is = new BufferedInputStream(new FileInputStream(giffile));
+			ImageInputStream iis = ImageIO.createImageInputStream(is);
+			
+			BufferedImage bufImage = ImageIO.read(iis);
+			
+			
+		//	Image image = ImageIO.read(is);
 
 			File outputImg = new File(gif.getParent().toString() + "//modifiedGif_" + gif.getName().toString() + ".gif");
-			FileImageOutputStream output = null;
+			ImageIO.write(bufImage, "gif", outputImg);
 
-			output = new FileImageOutputStream(outputImg);
-			writer.setOutput(output);
-			IIOImage img = new IIOImage((RenderedImage) gifbufimg, null, null);
-			writer.write(null, img, iwp);
 
-		} catch (Exception e) {
+	/*	} catch (Exception e) {
 			System.out.println(e);
 		}
-	}
-
-	public static BufferedImage getBufferedImage(File gif) {
-		Image gifimage = Toolkit.getDefaultToolkit().getImage(gif.getAbsolutePath());
-		int width = gifimage.getWidth(null);
-		System.out.println(width);
-		int height = gifimage.getHeight(null);
-		System.out.println(height);
-
-		BufferedImage newImage = new BufferedImage(width, height, 
-			BufferedImage.TYPE_INT_ARGB);
-			Graphics g = newImage.getGraphics();
-			g.drawImage(gifimage, width, height, null);
-			g.dispose();
-		
-		gifimage.getGraphics();
-		
-		return newImage;
-
+		*/
 	}
 }
