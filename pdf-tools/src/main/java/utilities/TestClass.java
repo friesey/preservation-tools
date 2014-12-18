d90e9d1e66877d6b94b5384032ce43632c3c49f2
friesey <y.friese@zbw.eu>
2014-12-18 14:32:39 +0100

    zip unpack method works, just fix file path issues (see TestClass)

--- pdf-tools/src/main/java/utilities/TestClass.java
+++ pdf-tools/src/main/java/utilities/TestClass.java
@@ -1,7 +1,14 @@
 package utilities;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import org.apache.commons.io.FilenameUtils;
 
@@ -11,20 +18,44 @@ public class TestClass {
 
 	public static void main(String args[]) throws Exception {
 
-		String folder = utilities.BrowserDialogs.chooseFolder();
+		// String folder = utilities.BrowserDialogs.chooseFolder();
+		String folder = "C://FileSample//zipSample";
 		if (folder != null) {
 			ArrayList<File> files = utilities.ListsFiles.getPaths(new File(folder), new ArrayList<File>());
-
-			folderforzips = utilities.FolderMethods.createnewFolder();
-
+		//	folderforzips = utilities.FolderMethods.createnewFolder();
 			for (int i = 0; i < files.size(); i++) {
 				String extension = FilenameUtils.getExtension(files.get(i).toString()).toLowerCase();
 				if (extension.equals("zip")) {
-					System.out.println(files.get(i).toString() + " recognised zip");
-					ArrayList<File> arrzips = new ArrayList<File>();
-					utilities.ListsFiles.unpackzip(files.get(i));
+
+					ZipFile zf = new ZipFile(files.get(i));
+					for (Enumeration<? extends ZipEntry> e = zf.entries(); e.hasMoreElements();) {
+						try {
+							ZipEntry entry = e.nextElement();
+							ziptofile(entry, zf);
+						} catch (Exception exc) {
+							System.out.println(exc);
+						}
+
+					}
 				}
 			}
 		}
+	}
+
+	private static void ziptofile(ZipEntry entry, ZipFile zf) throws IOException {
+		
+		//This method works!
+		
+		byte[] buffer = new byte[ 0xFFFF ];
+		InputStream in = zf.getInputStream(entry);
+		
+		String extension = FilenameUtils.getExtension(entry.toString()).toLowerCase();
+		
+		File newfile = new File ("D://Test//test." + extension);
+		OutputStream os = new FileOutputStream (newfile);
+        for ( int len; (len = in.read(buffer)) != -1; ) 
+            os.write( buffer, 0, len );       
+	in.close();
+	os.close();
 	}
 }
