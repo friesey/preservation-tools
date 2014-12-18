8bd3571eb27b6e31a0f0d240392ab1f59bb1bd9b
friesey <y.friese@zbw.eu>
2014-12-18 11:16:04 +0100

    begin to develop methods that look into zip files

--- pdf-tools/src/main/java/utilities/ListsFiles.java
+++ pdf-tools/src/main/java/utilities/ListsFiles.java
@@ -1,9 +1,20 @@
 package utilities;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.zip.*;
 
 import javax.swing.JOptionPane;
+
+import org.apache.commons.io.FilenameUtils;
+
+import com.google.common.io.Files;
+import com.google.common.io.InputSupplier;
 
 public class ListsFiles {
 	/**
@@ -14,26 +25,66 @@ public class ListsFiles {
 	 *
 	 */
 	public static ArrayList<File> getPaths(File file, ArrayList<File> list) {
-		
+
 		try {
-		if (file == null || list == null || !file.isDirectory())
+			if (file == null || list == null || !file.isDirectory())
+				return null;
+			File[] fileArr = file.listFiles();
+			for (File f : fileArr) {
+				// TODO If a folder is chosen that cannot be searched/read, e.
+				// g.
+				// C:/, the tool runs into issues
+				if (!f.isDirectory()) {
+					// should not add directories to the ArrayList of files
+					list.add(f);
+
+				} else {
+					getPaths(f, list);
+				}
+			}
+			return list;
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Most likely you tried to choose a folder like \"C:\" but you do not have the rights to read files there.", "Info Message", JOptionPane.PLAIN_MESSAGE);
 			return null;
-		File[] fileArr = file.listFiles();
-		for (File f : fileArr) {			
-			// TODO If a folder is chosen that cannot be searched/read, e. g.
-			// C:/, the tool runs into issues
-			if (!f.isDirectory()) {				
-				//should not add directories to the ArrayList of files
-				list.add(f);
-			} else {				
-				getPaths(f, list);
+		}
+	}
+
+	public static ArrayList<File> unpackzip(File zipfile) throws ZipException, IOException {
+		ArrayList<File> arrzips = new ArrayList<File>();
+		ZipFile zf = new ZipFile(zipfile);
+		for (Enumeration<? extends ZipEntry> e = zf.entries(); e.hasMoreElements();) {
+			try {
+			ZipEntry entry = e.nextElement();
+			
+			File entrytofile = ziptofile(entry, zf);
+			arrzips.add(entrytofile);
+			System.out.println(entry.getName());
+			}
+			catch (Exception exc) {
+				System.out.println (zipfile.toString() + exc);
 			}
 		}
-		return list;
+		zf.close();
+		// TODO Auto-generated method stub
+		return arrzips;
 	}
-		catch (Exception e) {		
-			JOptionPane.showMessageDialog(null, "Most likely you tried to choose a folder like \"C:\" but you do not have the rights to read files there.", "Info Message", JOptionPane.PLAIN_MESSAGE);
-		return null;		
+
+	private static File ziptofile(ZipEntry entry, ZipFile zf) throws IOException {
+		final InputStream zipStream = zf.getInputStream(entry);
+		byte[] buffer = new byte[zipStream.available()];
+		zipStream.read(buffer);
+		File targetFile =new File("C://FileSample//zipSample//targetFile.tmp"); //I do not like this solution
+		OutputStream outStream = new FileOutputStream(targetFile);
+		outStream.write(buffer);
+		outStream.close();
+		return targetFile;
 	}
-}
+
+	public static ArrayList<File> unpackjar(File jarfile) {
+		ArrayList<File> arrjars = new ArrayList<File>();
+		// TODO Auto-generated method stub
+		return arrjars;
+	}
+
+	// tar can be unpacked via http://tutego.de/go/tarcvs
 }
---
+++ pdf-tools/src/main/java/utilities/TestClass.java
@@ -0,0 +1,26 @@
+package utilities;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import org.apache.commons.io.FilenameUtils;
+
+public class TestClass {
+
+	public static void main(String args[]) throws Exception {
+
+		String folder = utilities.BrowserDialogs.chooseFolder();
+		if (folder != null) {
+			ArrayList<File> files = utilities.ListsFiles.getPaths(new File(folder), new ArrayList<File>());
+
+			for (int i = 0; i < files.size(); i++) {
+				String extension = FilenameUtils.getExtension(files.get(i).toString()).toLowerCase();
+				if (extension.equals("zip")) {
+					System.out.println(files.get(i).toString() + " recognised zip");
+					ArrayList<File> arrzips = new ArrayList<File>();
+					arrzips = utilities.ListsFiles.unpackzip(files.get(i));
+				}
+			}
+		}
+	}
+}
