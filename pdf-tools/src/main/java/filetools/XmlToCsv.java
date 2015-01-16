cde30bf85a768914820b7cc9064c928e54ca1c78
friesey <y.friese@zbw.eu>
2015-01-16 16:13:50 +0100

    implemented for 1 and 2

--- pdf-tools/src/main/java/filetools/XmlToCsv.java
+++ pdf-tools/src/main/java/filetools/XmlToCsv.java
@@ -2,6 +2,7 @@ package filetools;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -14,6 +15,14 @@ public class XmlToCsv {
 
 	static String SEPARATOR = ";";
 	static String MISSING_VALUE = "";
+	static PrintWriter outputCsv;
+
+	static String repository = "Repository";
+	static int criterium;
+	static String answer = "Answer";
+	static String links = "Links & References";
+
+	static BufferedReader xmlreader;
 
 	public static String examinedFolder;
 
@@ -22,61 +31,111 @@ public class XmlToCsv {
 		examinedFolder = utilities.BrowserDialogs.chooseFolder();
 		if (examinedFolder != null) {
 
-			BufferedReader xmlreader;
-
-			PrintWriter outputCsv = new PrintWriter(new FileWriter(examinedFolder + "//" + "outputCsv.csv"));
+			outputCsv = new PrintWriter(new FileWriter(examinedFolder + "//" + "outputCsv.csv"));
 
 			ArrayList<File> files = utilities.ListsFiles.getPaths(new File(examinedFolder), new ArrayList<File>());
 
-			String repository = "Repository";
-			String criterium = "Criterium";
-			String answer = "Answer";
-			String links = "Links & References";
-
-			outputCsv.println(repository + SEPARATOR + criterium + SEPARATOR + answer + SEPARATOR + links);
+			outputCsv.println(repository + SEPARATOR + "criterium" + SEPARATOR + answer + SEPARATOR + links);
 
 			for (int i = 0; i < files.size(); i++) {
 				String extension = FilenameUtils.getExtension(files.get(i).getCanonicalPath()).toLowerCase();
 				if (extension.equals("xml")) {
-
-					xmlreader = new BufferedReader(new FileReader(files.get(i)));
-
-					String line;
-					ArrayList<String> lineslist = new ArrayList<String>();
-					criterium = "1";
-
-					while (null != (line = xmlreader.readLine())) {
-						lineslist.add(line);
-					}
-
-					for (int j = 0; j < lineslist.size(); j++) {
-						String temp;
-						if (lineslist.get(j).contains("<Repository>")) {
-							
-							temp = lineslist.get(j).replace("<Repository>", "");
-							temp = temp.replace("</Repository>", "");
-							repository = temp;
-						}
-
-						// if (lineslist.get(j).contains("<Criterium Number=\"1\">")) {
-							
-							if (lineslist.get(j).contains("Applicant Entry")) {
-						
-							StringBuffer buffer1 = new StringBuffer();
-							while (!lineslist.get(j).equals("</Criterium>")) {
-								++j;								
-								buffer1.append(lineslist.get(j));
-							}
-							answer = buffer1.toString();
-						}
-
-					}
-						outputCsv.println(repository + SEPARATOR + criterium + SEPARATOR + answer + SEPARATOR + links);
+					getcriterium1(files.get(i));
 				}
-				
+			}
+
+			for (int i = 0; i < files.size(); i++) {
+				String extension = FilenameUtils.getExtension(files.get(i).getCanonicalPath()).toLowerCase();
+				if (extension.equals("xml")) {
+					getcriterium2(files.get(i));
+				}
 			}
 
 			outputCsv.close();
+		}
+	}
+
+	public static void getcriterium1(File file) throws IOException {
+		xmlreader = new BufferedReader(new FileReader(file));
+		String line = xmlreader.readLine();
+		ArrayList<String> lineslist = new ArrayList<String>();
+		criterium = 1;
+		String temp;
+		// repository = file.getName();
+
+		while (line != null) {
+			line = xmlreader.readLine();
+			lineslist.add(line);
+		}
+		for (int j = 0; j < lineslist.size(); j++) {
+
+			if (lineslist.get(j).contains("<Repository>")) {
+				temp = lineslist.get(j).replace("<Repository>", "");
+				temp = temp.replace("</Repository>", "");
+				repository = temp;
+			}
+
+			if (lineslist.get(j).contains("<Criterium Number=\"1\">")) {
+				System.out.println(lineslist.get(j));
+				StringBuffer buffer1 = new StringBuffer();
+				while (!lineslist.get(j).equals("</Criterium>")) {
+					++j;
+					buffer1.append(lineslist.get(j));
+				}
+				temp = buffer1.toString().replace("Statement of Compliance:", "");
+				temp = temp.replace("</Criterium>", "");
+				temp = temp.replace("Applicant Entry", "");
+				temp = temp.replace("Minimum Required Statement of Compliance:", "");
+				temp = temp.replace("Minimum Required", "");
+				temp = temp.replace("Self-assessment statement:", "");
+				temp = temp.replace("1. The data producer deposits the data in a data repository with sufficientinformation for others to assess the quality of the dataand compliance with disciplinary and ethical norms.", "");
+				answer = temp;
+
+				outputCsv.println(repository + SEPARATOR + criterium + SEPARATOR + answer + SEPARATOR + links);
+				return;
+
+			}
+		}
+	}
+
+	public static void getcriterium2(File file) throws IOException {
+		xmlreader = new BufferedReader(new FileReader(file));
+		String line = xmlreader.readLine();
+		ArrayList<String> lineslist = new ArrayList<String>();
+		criterium = 2;
+		String temp;
+		// repository = file.getName();
+
+		while (line != null) {
+			line = xmlreader.readLine();
+			lineslist.add(line);
+		}
+		for (int j = 0; j < lineslist.size(); j++) {
+
+			if (lineslist.get(j).contains("<Repository>")) {
+				temp = lineslist.get(j).replace("<Repository>", "");
+				temp = temp.replace("</Repository>", "");
+				repository = temp;
+			}
+
+			if (lineslist.get(j).contains("<Criterium Number=\"2\">")) {
+				System.out.println(lineslist.get(j));
+				StringBuffer buffer1 = new StringBuffer();
+				while (!lineslist.get(j).equals("</Criterium>")) {
+					++j;
+					buffer1.append(lineslist.get(j));
+				}
+				temp = buffer1.toString().replace("Statement of Compliance:", "");
+				temp = temp.replace("</Criterium>", "");
+				temp = temp.replace("Applicant Entry", "");
+				temp = temp.replace("Minimum Required Statement of Compliance:", "");
+				temp = temp.replace("Self-assessment statement:", "");
+				answer = temp;
+
+				outputCsv.println(repository + SEPARATOR + criterium + SEPARATOR + answer + SEPARATOR + links);
+				return;
+
+			}
 		}
 	}
 }
