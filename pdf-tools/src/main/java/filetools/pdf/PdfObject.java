0b9884cfd8142b0fb504ccd91c71aa4f31d3b54b
friesey <y.friese@zbw.eu>
2015-02-13 13:22:18 +0100

    test

---
+++ pdf-tools/src/main/java/filetools/pdf/PdfObject.java
@@ -0,0 +1,5 @@
+package filetools.pdf;
+
+public class PdfObject {
+
+}
---
+++ pdf-tools/src/main/java/filetools/pdf/PdfTestClass.java
@@ -0,0 +1,13 @@
+package filetools.pdf;
+
+public class PdfTestClass {
+	
+	public static void main(String args[]) throws Exception {
+		
+		PdfObject testPdf = new PdfObject();
+		
+		
+		
+	}
+
+}
