dbd7b663af06b2f78221ec16bf27cd243f5d6250
friesey <y.friese@zbw.eu>
2015-02-13 13:49:17 +0100

    created new Pdf Object

--- pdf-tools/src/main/java/filetools/pdf/PdfObject.java
+++ pdf-tools/src/main/java/filetools/pdf/PdfObject.java
@@ -1,5 +1,44 @@
 package filetools.pdf;
 
-public class PdfObject {
+import java.util.regex.Pattern;
 
+public class PdfObject {	
+	
+	String path;
+	String name;
+	
+	public void setPath (String newPath){
+		path = newPath;
+	}
+
+	public String getPath() {
+		return path;
+	}
+	
+	public String setName(String path) {								
+		String[] parts = path.toString().split(Pattern.quote("\\"));	
+		name = parts[parts.length-1]; //filename including extension		
+		String[] segs = name.split(Pattern.quote("."));
+		name = segs[segs.length - 2];						
+		return name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	//encryption
+	//size
+	//number of lines
+	//pdf ok
+	//pdf valid (but do not use jhove here)
+	//pdf version?
+	//pdf a?
+	
+	// get xmp metadata
+	
+	//what is available under pdfAnalysis
+	
+	
+	
 }
--- pdf-tools/src/main/java/filetools/pdf/PdfTestClass.java
+++ pdf-tools/src/main/java/filetools/pdf/PdfTestClass.java
@@ -4,8 +4,13 @@ public class PdfTestClass {
 	
 	public static void main(String args[]) throws Exception {
 		
-		PdfObject testPdf = new PdfObject();
+		PdfObject testPdf = new PdfObject();		
+		String newPath = utilities.BrowserDialogs.chooseFile();		
+		testPdf.setPath(newPath);
+		testPdf.setName(testPdf.path);
 		
+		System.out.println(testPdf.getPath());
+		System.out.println(testPdf.getName());
 		
 		
 	}
